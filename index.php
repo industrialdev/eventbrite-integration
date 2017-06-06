211d6ab773c144851be1b8811044cc39c861171a
Caio Bianchi <cbianchi@industrialagency.ca>
2017-06-06 15:17:56 -0400

    Organize test code into a working class

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
 <?php
 // Initialize Composer Libraries
 require_once "vendor/autoload.php";
-// Require EventBrite SDK (not Composer-friendly =\);
-require_once "vendor/eventbrite/eventbrite-sdk-php/HttpClient.php";
-// Simple custom Guzzle Wrapper for the EventBrite API
-// use Industrialdev\Eventbrite\Eventbrite;
+use Industrialdev\Eventbrite\Eventbrite;
 
-// Test Data
-$token = 'S5BPGHXXE2M4FK3JKJPY';
-$test_event_id = '35146505143';
+$eb = new Eventbrite();
 
-$client = new HttpClient($token);
-
-// Map of roles
-$roles = [];
-
-// Event Data
-$event = $client->get_event($test_event_id);
-d($event);
-
-// Access Codes
-$access_codes = $client->get_event_access_codes($test_event_id);
-d($access_codes);
+$events = $eb->get_event('35146505143');
+d($events);
--- src/Eventbrite.php
+++ src/Eventbrite.php
@@ -6,11 +6,17 @@
  */
 namespace Industrialdev\Eventbrite;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Exception\BadResponseException;
-use Psr\Http\Message\ResponseInterface;
+use Dotenv\Dotenv;
 use Exception;
+
+// Import Eventbrite SDK
+require_once "vendor/eventbrite/eventbrite-sdk-php/HttpClient.php";
+
+// Set up ENV vars
+$realpath = realpath(__DIR__ . '/..');
+$dotenv = new Dotenv($realpath);
+$dotenv->load();
+$dotenv->required(['API_APP_KEY', 'API_JWT_SECRET', 'PERSON_ID', 'EVENTBRITE_TOKEN']);
 
 /**
  * Class Eventbrite
@@ -19,240 +25,74 @@ use Exception;
  */
 class Eventbrite
 {
-    /**
-     * The current version of this library.
-     */
-    const VERSION = '1.0';
-    /**
-     * An array of valid HTTP verbs.
-     */
-    private static $valid_verbs = ['get', 'post', 'put', 'patch', 'delete'];
-    /**
-     * The API endpoint to get the current user's details.
-     */
-    const CURRENT_USER_ENDPOINT = 'users/me/';
-    /**
-     * @var string The Eventbrite OAuth token.
-     */
-    private $token;
-    /**
-     * @var \GuzzleHttp\Client
-     */
-    private $client;
-    /**
-     * @var \GuzzleHttp\Psr7\Request
-     */
-    private $last_request = null;
-    /**
-     * @var \Psr\Http\Message\ResponseInterface
-     */
-    private $last_response = null;
-    /**
-     * @param string $token
-     *   The OAuth token to authenticate the request
-     * @param array  $config
-     *   An array of Guzzle config options so that everything is configurable.
-     *
-     * @throws \Exception
-     */
-    public function __construct($token, $config = [])
+
+    const TEST_EVENT_ID = '35146505143';
+
+    public function eb_client()
     {
-        $default_config = [
-            'base_uri' => 'https://www.eventbriteapi.com/v3/',
-            // Turn exceptions off so we can handle the responses ourselves.
-            'exceptions' => false,
-            'timeout' => 30,
-        ];
-        $config = array_merge($default_config, $config);
-        // Add this last so it's always there and isn't overwritten.
-        $config['headers']['User-Agent'] = 'Industrialdev\Eventbrite v' . self::VERSION . ' ' . \GuzzleHttp\default_user_agent();
-        if (!empty($token)) {
-            $this->token = $token;
-            // Set the authorisation header.
-            $config['headers']['Authorization'] = 'Bearer ' . $this->token;
-            $this->client = new Client($config);
-        } else {
-            throw new \Exception('An OAuth token is required to connect to the Eventbrite API.');
-        }
+        return $this->eb_client = new \HttpClient(getenv('EVENTBRITE_TOKEN'));
     }
-    /**
-     * Make the call to Eventbrite, only synchronous calls at present.
-     *
-     * @param string $verb
-     * @param string $endpoint
-     * @param array  $options
-     *
-     * @return array|mixed|\Psr\Http\Message\ResponseInterface
-     * @throws \Exception
-     */
-    public function call($verb, $endpoint, $options = [])
+
+    public function wicket_client()
     {
-        if ($this->validMethod($verb)) {
-            // Get the headers and body from the options.
-            $headers = isset($options['headers']) ? $options['headers'] : [];
-            $body = isset($options['body']) ? $options['body'] : null;
-            $pv = isset($options['protocol_version']) ? $options['protocol_version'] : '1.1';
-            // Make the request.
-            $request = new Request($verb, $endpoint, $headers, $body, $pv);
-            // Save the request as the last request.
-            $this->last_request = $request;
-            // Send it.
-            $response = $this->client->send($request, $options);
-            if ($response instanceof ResponseInterface) {
-                // Set the last response.
-                $this->last_response = $response;
-                // If the caller wants the raw response, give it to them.
-                if (isset($options['parse_response']) && $options['parse_response'] === false) {
-                    return $response;
-                }
-                $parsed_response = $this->parseResponse($response);
-                return $parsed_response;
-            } else {
-                // This only really happens when the network is interrupted.
-                throw new BadResponseException('A bad response was received.',
-                    $request);
-            }
-        } else {
-            throw new \Exception('Unrecognised HTTP verb.');
-        }
+        return $this->wicket_client = new Wicket\Client(
+            getenv('API_APP_KEY'),
+            getenv('API_JWT_SECRET'),
+            'https://api.wicket.io'
+        );
+        $wicket = self::wicket_client();
+        $wicket->authorize(getenv('PERSON_ID'));
     }
-    /**
-     * A slightly abstracted wrapper around call().
-     *
-     * This essentially splits the call options array into different parameters
-     * to make it more obvious to less advanced users what parameters can be
-     * passed to the client.
-     *
-     * @param string $verb
-     * @param string $endpoint
-     * @param null   $params
-     * @param null   $body
-     * @param null   $headers
-     * @param array  $options
-     *
-     * @return array|mixed|\Psr\Http\Message\ResponseInterface
-     * @throws \Exception
-     */
-    public function makeRequest($verb, $endpoint, $params = null, $body = null, $headers = null, $options = [])
+
+    public function get_event(string $event_id): array
     {
-        // We can only have one body, so overwrite it if it exists.
-        if ($body !== null) {
-            $options['body'] = $body;
-        }
-        // Merge the mergeable arrays if necessary.
-        $mergeable = [
-            'query' => $params,
-            'headers' => $headers,
+        $client = self::eb_client();
+        return $client->get_event($event_id);
+    }
+
+    public function get_access_codes(string $event_id): array
+    {
+        $client = self::eb_client();
+        return $client->get_event_access_codes($event_id);
+    }
+
+    public function create_access_codes(string $event_id, array $ticket_id, array $opts): array
+    {
+        $opts = [
+            'access_code.code'               => 'CODE'.mt_rand(0, 100), // Actual access code
+            'access_code.ticket_ids'         => $ticket_id, // Ticket Type
+            'access_code.quantity_available' => 1, // Number of uses.
         ];
-        foreach ($mergeable as $key => $value) {
-            if ($value !== null) {
-                if (!isset($options[$key])) {
-                    $options[$key] = [];
-                }
-                $options[$key] = array_merge($options[$key], $value);
+
+        $client = self::eb_client();
+        return $client->post_event_access_codes($event_id, $opts);
+    }
+
+    public function create_event_url($event_id, $access_code): array
+    {
+        $event = self::get_event($event_id);
+        $codes = self::get_access_codes($event_id);
+
+        $url = $event['url'];
+
+        $urls = [];
+        if(!empty($codes)) {
+            foreach($codes['access_codes'] as $code) {
+                $urls[] = "${url}?discount=${code['code']}#tickets";
             }
         }
-        // Make the call.
-        return $this->call($verb, $endpoint, $options);
+        return $urls;
     }
-    /**
-     * Checks if the HTTP method being used is correct.
-     *
-     * @param string $http_method
-     *
-     * @return bool
-     */
-    public function validMethod($http_method)
+
+    public function get_wicket_roles() {
+        $wicket = self::wicket_client();
+        $wicket->authorize(getenv('PERSON_ID'));
+    }
+
+    public function get_mapped_roles()
     {
-        if (in_array(strtolower($http_method), self::$valid_verbs)) {
-            return true;
-        }
-        return false;
+        // Map of roles
+        $roles = [];
     }
-    /**
-     * Parses the response from
-     *
-     * @param \Psr\Http\Message\ResponseInterface $response
-     *
-     * @return array
-     */
-    public function parseResponse(ResponseInterface $response)
-    {
-        $body = $response->getBody()->getContents();
-        return [
-            'code' => $response->getStatusCode(),
-            'headers' => $response->getHeaders(),
-            'body' => ($this->isValidJson($body)) ? json_decode($body,
-                true) : $body,
-        ];
-    }
-    /**
-     * Checks a string to see if it's JSON. True if it is, false if it's not.
-     *
-     * @param string $string
-     *
-     * @return bool
-     */
-    public function isValidJson($string)
-    {
-        if (is_string($string)) {
-            json_decode($string);
-            return (json_last_error() === JSON_ERROR_NONE);
-        }
-        return false;
-    }
-    /**
-     * Checks if the class can connect to the Eventbrite API.
-     *
-     * Checks if we can connect to the API by calling the user endpoint and
-     * checking the response code. If the response code is 2xx it returns true,
-     * otherwise false.
-     *
-     * @return bool
-     */
-    public function canConnect()
-    {
-        $data = $this->get(self::CURRENT_USER_ENDPOINT);
-        if (strpos($data['code'], '2') === 0) {
-            return true;
-        }
-        return false;
-    }
-    /**
-     * Provides shortcut methods named by HTTP verbs.
-     *
-     * Provides shortcut methods for GET, POST, PUT, PATCH and DELETE.
-     *
-     * @param string $method
-     * @param array  $args
-     *
-     * @return array|mixed|\Psr\Http\Message\ResponseInterface
-     * @throws \Exception
-     */
-    public function __call($method, $args) {
-        if ($this->validMethod($method)) {
-            array_unshift($args, $method);
-            return call_user_func_array(array($this, 'makeRequest'), $args);
-        } else {
-            throw new \BadMethodCallException('Method not found in class.');
-        }
-    }
-    /**
-     * Returns the last request object for inspection.
-     *
-     * @return \GuzzleHttp\Psr7\Request
-     */
-    public function getLastRequest()
-    {
-        return $this->last_request;
-    }
-    /**
-     * Returns the last response object for inspection.
-     *
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function getLastResponse()
-    {
-        return $this->last_response;
-    }
+
 }
