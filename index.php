2ba95b99447916b1e4683f49ab638c3a1d8e9865
Caio Bianchi <cbianchi@industrialagency.ca>
2017-06-19 11:12:23 -0400

    Implement authorization that will allow us to see what events the user has registered to

--- index.php
+++ index.php
@@ -6,14 +6,20 @@ use Industrialdev\Eventbrite\Eventbrite;
 // Creates Eventbrite SDK instance
 $eb = new Eventbrite(getenv('EVENTBRITE_TOKEN'));
 
-$code          = $_GET('code');
+$code          = $_GET['code'];
 $client_key    = getenv('CLIENT_KEY');
 $client_secret = getenv('CLIENT_SECRET');
-$app_key       = getenv('EVENTBRITE_TOKEN');
+$app_key       = getenv('APP_KEY');
 
-// Gets the
-$auth_url = $eb->createAuthorizeUrl($client_key);
+// Asks for user permission
+if(empty($code)) {
+    $auth_url = $eb->createAuthorizeUrl($client_key);
+    Header("Location: ${auth_url}");
+}
+// Authorize and get the access token
+else {
+    $oauth = $eb->authorize($code, $client_secret, $client_key);
+    // Authorization: Bearer ${token}
+    d($oauth);
+}
 
-Header($auth_url);
-
-// $oauth = $eb->authorize($code, $client_secret, $app_key);
